6ea6f164afa1a8d599adc62b864b2c8f7fadfd72
Malia Cortez <mookiethompson17@gmail.com>
2020-03-10 17:09:18 -0700

    Add first parts of app, intro

--- app.R
+++ app.R
@@ -5,3 +5,16 @@ library(ggplot2)
 library(maps)
 
 source("project.R")
+
+ui <- fluidPage (
+  h1("Study on Economic Status and Education Rates by Country"),
+  
+  h2("Data Introduction"),
+  p()
+)
+
+server <- function(input, output) {
+  
+}
+
+shinyApp(ui = ui, server = server)
