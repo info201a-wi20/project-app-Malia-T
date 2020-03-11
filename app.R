5708911f06eecc4a2fdaf6efb71f14ae54f8a0c0
mz4201 <47264851+mz4201@users.noreply.github.com>
2020-03-10 17:02:15 -0700

    added the title of the app

--- app.R
+++ app.R
@@ -5,3 +5,21 @@ library(ggplot2)
 library(maps)
 
 source("project.R")
+
+#UI layouts (allows for nesting!)
+#static content
+#control widgets
+#reactive outputs
+
+ui <- fluidPage(
+  titlePanel("Economy Growth Rates of Countries versus Graudation Rates of Higher Education")
+  
+)
+
+server <- function(input, output) {
+  # use values from `input` list
+  # assign values to `output` list
+  # we'll fill this in soon
+}
+
+shinyApp(ui = ui, server = server)
