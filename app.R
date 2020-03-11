ff9eba7e025e1bb95452bb7b36e88e9902e4213c
mz4201 <47264851+mz4201@users.noreply.github.com>
2020-03-10 19:09:11 -0700

    added one checkbox option

--- app.R
+++ app.R
@@ -8,17 +8,25 @@ source("project.R")
 
 ui <- fluidPage (
   titlePanel("Study on Economic Status and Education Rates by Country"),
-  
   h2("Data Introduction"),
-  p()
+  p(),
+  show_edu_input
+)
+
+show_edu_input <- checkboxInput(
+  inputId = "edu_graph",
+  label = "Show Education Graph",
+  value = TRUE
 )
 
 server <- function(input, output) {
-  
-#UI layouts (allows for nesting!)
-#static content
-#control widgets
-#reactive outputs
+  if(input_list$edu_graph == TRUE){
+    the_plot <- the_plot + geom_smooth(mapping = aes_string(
+      x = input_list$feature_choice,
+      y = "price",
+      color = "cut"
+    ),se = FALSE)
+  }
 }
 
 shinyApp(ui = ui, server = server)
