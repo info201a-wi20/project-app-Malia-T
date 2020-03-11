a985d5f7f384e8d49c5354291ce92b44f1a71f0c
Serah Prakkat <serahprakkat@Serahs-MacBook-Pro.local>
2020-03-10 23:50:15 -0700

    added introductions

--- app.R
+++ app.R
@@ -5,20 +5,25 @@ library(ggplot2)
 library(maps)
 
 source("project.R")
-<<<<<<< HEAD
+
+
+home <- tabPanel(
+  "Home",
+  titlePanel("Introduction")
+)
 #Data introduction
 
 #Through this project, we are attempting to draw relationships and comparisons between these two variables; economy rates of countries and higher rates of education in different countries.
 
 ###Some of the questions that will facilitate in drawing these comparisons between the two variables inlcude:
 #Is there a relationship between economic status of a country and their graduation rates?
- #This question is aimed to help us finally analyze and answer if there is a relationship between the two factors; if there is a correlation between the two, and if so, the type of correlation (positive, negative).
+#This question is aimed to help us finally analyze and answer if there is a relationship between the two factors; if there is a correlation between the two, and if so, the type of correlation (positive, negative).
 #Rank the education rate from highest to lowest among countries worldwide, and also show their economic trend.
- #This question will help us understand which country has the highest rates of highest education and which has the lowest rates.
+#This question will help us understand which country has the highest rates of highest education and which has the lowest rates.
 #How does the correlation of US higher education rates vs. economy look like with respect to US events?
- #This question will help us understand how these values compare to how well the country is faring.
+#This question will help us understand how these values compare to how well the country is faring.
 #Which regions tend to have a higher GDP? Higher graduation rates? What could these results entail?
- #This will help us understand if there is a certain region where more affluent individuals use overseas bank accounts to store money, for example. These findings could prompt many more questions.
+#This will help us understand if there is a certain region where more affluent individuals use overseas bank accounts to store money, for example. These findings could prompt many more questions.
 #We chose this topic, as we believe that education is a very essential tool to a person's overall success in life irrespective of age. Just like every human being requires oxygen to survive, education too is very essential to survive in this world. Education provides people the knowledge and skills they need in order to survive. By analyzing these questions relating to education and how the economy rates of countries affect education - we hope to be able to better understand which countries are doing well in terms of economy and higher rates of education, and which countries are not doing well in these aspects. By understanding these aspects, we as students can work on spreading awareness regarding countries that are low on higher education rates and work in small ways that impact those countries and improve the overall higher educational rates. Thus, we believe that the topic chosen is important.
 
 #Data Description
@@ -26,12 +31,6 @@ source("project.R")
 #The data was collected and distributed by the respective organizations.
 #**World Bank Data: https://data.worldbank.org/indicator/NY.GDP.PCAP.CD?end=2018&start=2018&view=bar**
 #**OECD Data: https://stats.oecd.org/Index.aspx?datasetcode=EAG_GRAD_ENTR_RATES**
-  
-=======
-home <- tabPanel(
-  "Home",
-  titlePanel("Introduction")
-)
 
 #correlation
 #select by year
