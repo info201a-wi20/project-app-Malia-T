896633ed0cbb13d6ecbda978bd66af931dcc71ae
mz4201 <47264851+mz4201@users.noreply.github.com>
2020-03-10 19:45:20 -0700

    adjusted the code for the panels

--- app.R
+++ app.R
@@ -5,27 +5,56 @@ library(ggplot2)
 library(maps)
 
 source("project.R")
+home <- tabPanel(
+  "Home",
+  titlePanel("Introduction")
+)
+
+#correlation
+#select by year
+#select by country
+q1 <- tabPanel(
+  "Econ. Status & Grad Rates"
+)
+q2 <- tabPanel(
+  "Education & Econ. Status Ranked"
+)
+q3 <- tabPanel(
+  "US Correlation & US Events"
+)
+
+#worldmap
+#by year
+#two maps to choose if want to see both
+q4 <- tabPanel(
+  "Worldwide GDP & Graduation Rate",
+  sidebarLayout(
+    sidebarPanel(
+      textInput(inputId = "username", label = "What is your name?")
+    ),
+    mainPanel(
+      h3("Primary Content"),
+      p("Plots, data tables, etc. would go here")
+    )
+  )
+)
 
 ui <- fluidPage (
   titlePanel("Study on Economic Status and Education Rates by Country"),
   navbarPage(
-    title = "",
-    tabPanel(title = "Home"),
-    tabPanel(title = "Econ. Status & Grad Rates"),
-    tabPanel(title = "Education & Econ. Status Ranked"),
-    tabPanel(title = "US Correlation & US Events"),
-    tabPanel(title = "Worldwide GDP & Graduation Rate")
-  ),
-  
-  h2("Data Introduction")
+    title = "Info 201, AH Team 1",
+    home,
+    q1,
+    q2,
+    q3,
+    q4
+  )
 )
+
+
 
 server <- function(input, output) {
   
-  #UI layouts (allows for nesting!)
-  #static content
-  #control widgets
-  #reactive outputs
 }
 
 shinyApp(ui = ui, server = server)
